450c8d3c9ddc68e85d1e5e5def6418d1272a4ff8
Willy <wscatao@gmail.com>
2020-09-07 14:02:29 -0300

    Manipulando data

--- src/pages/MedicalRecord.jsx
+++ src/pages/MedicalRecord.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-
-import MedicalContext from '../context/MedicalContext';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import moment from 'moment';
+
+import MedicalContext from '../context/MedicalContext';
 
 const MedicalRecord = () => {
   const { medicalRecords } = useContext(MedicalContext);
 
   const dateTreatment = (arrRecords) => {
-    console.log('lista de prontuários', arrRecords);
+    console.log(arrRecords)
+    if (arrRecords[0] === Error) return <li>Houve um erro</li>;
     // Função criada para separar data e hora utilizando Regex e criar um novo array de prontuários com data e hora separado.
     const createdAt = arrRecords.map((dates) => dates.created_at);
     const dateRegex = /[0-9]+-[0-9]+-[0-9]+/g;
@@ -17,6 +19,12 @@ const MedicalRecord = () => {
     const dates = [];
     const times = [];
     const newMedicalRecord = [];
+
+    const teste = arrRecords[0].created_at.match(dateRegex);
+      console.log('antes de passar no moment', teste[0])
+      console.log('tipo de teste', typeof(teste[0]))
+    const newteste = teste.split('-');
+      console.log('depois de passar no moment com parser', newteste)
 
     createdAt.forEach((created) => {
       dates.push(created.match(dateRegex));
@@ -63,6 +71,7 @@ const MedicalRecord = () => {
           color="primary"
           component={Link}
           to="/cadastro"
+          fullWidth
         >
           Adicionar novo prontuário
         </Button>
